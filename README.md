2314be314826705c81679475ec9be0f226c6b486
smith961 <smithbabawande@gmail.com>
2023-08-19 18:57:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Project Title: Joke Generator
+Date: 2023/08/19
+Author : Smith AbdulHakeem
+Description:
+it is a program that generates joke by fetching the data from an API.
+Installation
+You will use git clone to be able to download the documents in the github
+Installation Requirements
+Git
+Git clone: git@github.com:smith961/code_challenge_2.git
+Live Link
+Technologies Used:
+HTML,
+CSS,
+JavaScript,
+Github.
+Support and contact details : github.com/smith961
+License:
+The content of this site is license under the MIT license
+Copyright (c) 2018.
---
+++ index.js
@@ -0,0 +1,38 @@
+const btnEl = document.querySelector('#btn');
+const jokeEl = document.querySelector('#joke');
+
+const apiKey = "EJ+LyI1GkTXhZ1sGSq5hGQ==ss2SmigTLF6zyAom";
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+const options ={
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
+
+async function getjoke(){
+ try {
+    
+        jokeEl.textContent = "Updating...";
+        btnEl.disabled = true;
+        btnEl.textContent = "Loading..."
+        
+        const response = await fetch(apiURL, options)
+        const data = await response.json();
+       
+        jokeEl.textContent = data[0].joke;
+        btnEl.disabled = false;
+        btnEl.textContent = "Tell me a joke";
+        
+    
+    }
+catch (error) {
+        jokeEl.textContent = "An error happened, try again later";
+        btnEl.disabled = false;
+        btnEl.textContent = "Tell me a joke";
+     }
+    
+} 
+
+btnEl.addEventListener("click", getjoke)
